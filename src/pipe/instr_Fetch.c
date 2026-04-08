10ed283045485f9a0bff507eac57932a5d933a96
anushkag123 <anushkag0831@gmail.com>
2026-04-08 03:10:44 -0500

    no ins halt

--- src/pipe/instr_Fetch.c
+++ src/pipe/instr_Fetch.c
@@ -191,9 +191,9 @@ comb_logic_t fetch_instr(f_instr_impl_t *in, d_instr_impl_t *out) {
         imem_err = false;
     } else {
         // Student TODO
-        if(F_in->status == STAT_INS) {
-            return;
-        }
+        // if(F_in->status == STAT_INS) {
+        //     return;
+        // }
         
         imem(current_PC, &out->insnbits, &imem_err);
 
