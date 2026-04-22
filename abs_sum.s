c35e227fe598f46f8250fb08df36ba7c7d353b43
Khushi Bhalani <khushib@cs.utexas.edu>
2026-04-22 14:17:09 -0500

    straight line abs

--- abs_sum.s
+++ abs_sum.s
@@ -9,28 +9,19 @@
 // x1: size of array (32)
 abs_sum:
     // Modify below here
-    movz    x2, #0   /           // Running sum.
+    movz    x2, #0   /          // Running sum.
     subs    x1, x1, xzr         // Check if there are elements left in array to sum.
-    b.neq    .loop               // If not, we are done.
+    b.ne    .loop               // If not, we are done.
     b .done
 .loop:
     ldur    x3, [x0, #0]        // Load element from memory.
-
-    cmp     x3, xzr             // Check if element is positive
-    b.ge    .positive
-
-.negative:
-    subs    x3, xzr, x3         // Element is negative, negate it.
-                                // Fall through to positive case.
-
-.positive:
-    adds    x2, x2, x3          // Element is positive, add it to sum.
-    
-.end:
     add     x0, x0, #8          // Increment pointer.
-    sub     x1, x1, #1          // Decrement remaining size.
+    asr     x4, x3, #63
+    eor     x3, x3, x4
+    sub     x3, x3, x4
+    add     x2, x2, x3
     subs    x1, x1, xzr
-    b.neq    .loop               // Continue loop.
+    b.ne    .loop               // Continue loop.
 
 .done:
     adds    x0, xzr, x2         // Move sum into x0.
