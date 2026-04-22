659b6dcbe52bf50f562d778fd19fc74f8165cf80
Khushi Bhalani <khushib@cs.utexas.edu>
2026-04-22 14:23:21 -0500

    fixed loop increment

--- abs_sum.s
+++ abs_sum.s
@@ -9,24 +9,22 @@
 // x1: size of array (32)
 abs_sum:
     // Modify below here
-    movz    x2, #0   /          // Running sum.
+    movz    x2, #0              // Running sum.
     subs    x1, x1, xzr         // Check if there are elements left in array to sum.
     b.ne    .loop               // If not, we are done.
-    b .done
+    b       .done
 .loop:
     ldur    x3, [x0, #0]        // Load element from memory.
     add     x0, x0, #8          // Increment pointer.
     asr     x4, x3, #63
     eor     x3, x3, x4
     sub     x3, x3, x4
+    
     add     x2, x2, x3
-    subs    x1, x1, xzr
+    subs    x1, x1, #1
     b.ne    .loop               // Continue loop.
 
 .done:
-    adds    x0, xzr, x2         // Move sum into x0.
+    add    x0, xzr, x2         // Move sum into x0.
     ret
-    //abs of x
-    //y = x >> 31
-    //(x ^ y) - y
 .size   abs_sum, .-abs_sum
